c0c0f20de35aa827e299e0a63fab7fefc19f739e
Vincent Daribo <88313116+Vincent-DARIBO@users.noreply.github.com>
2025-11-23 10:57:28 +0100

    disabled ssr

--- app/hooks/useNotifications.ts
+++ app/hooks/useNotifications.ts
@@ -1,9 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
-import { NotificationService } from '../services/NotificationService';
+import { useQuery } from "@tanstack/react-query";
+import { NotificationService } from "../services/NotificationService";
 
 export default function useNotifications() {
   const { data, isLoading, error, refetch } = useQuery({
-    queryKey: ['notifications'],
+    queryKey: ["notifications"],
     queryFn: () => NotificationService.getNotifications(),
   });
 
